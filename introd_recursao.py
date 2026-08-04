d9ea3f76195aad0a4580edd894ab31c17f338cba
Geovana Cardoso <geovanamscardoso183@gmail.com>
2026-08-04 10:18:20 -0300

    Implement factorial and power functions
    Added functions for factorial and power calculations using both iterative and recursive methods.

---
+++ introd_recursao.py
@@ -0,0 +1,33 @@
+def fatorial_it(n):
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
+    return resultado
+
+print(fatorial_it(10))
+'''%time fatorial_it(1500)
+CPU times: total: 0 ns
+Wall time: 363 μs'''
+
+
+def fatorial(n):
+    # Caso base / condição de parada
+    if n == 0:
+        return 1
+
+    # Passo recursivo
+    return n * fatorial(n - 1)
+
+print(fatorial(10))
+'''%time fatorial(1500)
+CPU times: total: 0 ns
+Wall time: 497 μs'''
+
+def potencia(base, expoente):
+    # Caso base / condição de parada
+    if expoente == 0:
+        return 1
+    # Passo recursivo
+    return base * (potencia(base, expoente - 1))
+
+print(potencia(3,5))
