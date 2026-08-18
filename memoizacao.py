ae7089302ba579627e51c87e148656646a258127
Geovana Cardoso <geovanamscardoso183@gmail.com>
2026-08-18 09:38:00 -0300

    Implement memoized Fibonacci function
    Added a memoized Fibonacci function to improve performance.

--- memoizacao.py
+++ memoizacao.py
@@ -15,3 +15,17 @@ def fatorial_memo(n, memo=None):
 Wall time: 5.01 μs'''
 
 print(fatorial_memo(10))
+
+def fib(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
+
+print(fib(10))
+'''%time fib(10)
+CPU times: total: 0 ns
+Wall time: 3.58 μs
+Out[12]: 55'''
